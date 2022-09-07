45274c74e2f6de76a5087432640857596c8ea167
alexeiMikhail <leleuxc@gmail.com>
2022-09-07 10:43:27 -0500

    Create paddle class with draw method

--- script.js
+++ script.js
@@ -5,6 +5,7 @@ const resize = () => {
     canvas.width = window.innerWidth;
     canvas.height = window.innerHeight;
     console.log("Canvas resized.");
+    drawBackground();
 }
 
 resize();
@@ -21,9 +22,34 @@ function fullscreen() {
 
 main();
 
-
 ///////////////////////////
 
 function main() {
-    return
+    drawBackground();
 }
+
+function drawBackground() {
+    ctx.fillStyle = "black";
+    ctx.fillRect(0, 0, canvas.width, canvas.height);
+}
+
+//////////////////////////
+
+class Paddle {
+    constructor(x, y, w, h) {
+        this.x = x;
+        this.y = y;
+        this.w = w;
+        this.h = h;
+    }
+    draw() {
+        ctx.fillStyle = "white";
+        ctx.fillRect(this.x, this.y, this.w, this.h);
+    }
+}
+
+paddle1 = new Paddle(20, 20, 20, 100);
+paddle2 = new Paddle(canvas.width - 20 - 20, 20, 20, 100);
+
+paddle1.draw();
+paddle2.draw();
